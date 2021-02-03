8931bb982ac3ff3b1ce58277be932354e132cb52
Allison Pedro <allisondkk@gmail.com>
2021-02-03 12:36:29 -0300

    update

---
+++ String/script.js
@@ -0,0 +1,4 @@
+var nome = window.prompt('qual o seu nome ?')
+document.write(`Olá ${nome}, Seu nome tem ${nome.length} letras <br/>`)
+document.write(`Seu nome em maiúsculas é ${nome.toUpperCase()} <br/>`)
+document.write(`Seu nome em minuscula é ${nome.toLowerCase()} <br/>`)
--- tratamento-de-dados-2/script.js
+++ tratamento-de-dados-2/script.js
@@ -1,4 +1,36 @@
-var n1 = window.prompt('Digite o primeiro numero')
-var n2 = window.prompt('Digite o segundo numero')
-var soma =  n1 + n2
-window.alert('A soma dos valores é ' + soma)
+
+// //parseFloat
+ var n1 = Number.parseFloat(window.prompt('Digite o primeiro numero')) 
+ var n2 = Number.parseFloat(window.prompt('Digite o segundo numero'))
+ var soma =  n1 + n2
+ window.alert('A soma dos valores é ' + soma)
+
+// //parseInt
+ var n1 = Number.parseInt(window.prompt('Digite o primeiro numero')) 
+ var n2 = Number.parseInt(window.prompt('Digite o segundo numero'))
+ var soma =  n1 + n2
+ window.alert('A soma dos valores é ' + String(soma))
+
+// //Number
+ var n1 = Number(window.prompt('Digite o primeiro numero')) 
+ var n2 = Number(window.prompt('Digite o segundo numero'))
+ var soma =  n1 + n2
+ window.alert('A soma dos valores é ' + soma.toString())
+
+
+//formatando String
+var aluno = String(window.prompt('nome do aluno')) 
+var n1 = Number(window.prompt('Digite a primeira nota')) 
+var n2 = Number(window.prompt('Digite a segunda nota'))
+var media =  (n1 + n2)/2
+var texto = ("O aluno "+aluno+ ", tirou a media "+media+" como final ")
+window.alert(String(texto))
+
+//use crase pra dá certo pelo amor de Deus
+//formatando String com Crase
+var aluno = String(window.prompt('nome do aluno')) 
+var n1 = Number(window.prompt('Digite a primeira nota')) 
+var n2 = Number(window.prompt('Digite a segunda nota'))
+var media =  (n1 + n2)/2
+var texto = (`O aluno ${aluno}, tirou a media ${media} como final `)
+window.alert(String(texto))
