7d716bd5ec87ac437e81be3d697e76b030be3def
Allison Pedro <allisondkk@gmail.com>
2021-02-09 12:40:14 -0300

    update

--- exercisios/ex02/script.js
+++ exercisios/ex02/script.js
@@ -8,7 +8,43 @@ function verificar() {
     }else{
         var fsex = document.getElementsByName('radsex') //0 ou 1
         var idade = ano - Number(fano.value)
-        res.innerHTML = `Idade calculada: ${idade}`
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                //criança
+                img.setAttribute('src','foto-bebe-m.png')
+            }else if (idade <21) {
+                //jovem
+                img.setAttribute('src','foto-jovem-m.png')
+            }else if (idade < 50) {
+                //adulto
+                img.setAttribute('src','foto-adulto-m.png')
+            }else{
+                //Idoso
+                img.setAttribute('src','foto-idoso-m.png')
+            }
+        }else if (fsex[1].checked) {
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                //criança
+                img.setAttribute('src','foto-bebe-f.png')
+            }else if (idade <21) {
+                //jovem
+                img.setAttribute('src','foto-jovem-f.png')
+            }else if (idade < 50) {
+                //adulto
+                img.setAttribute('src','foto-adulto-f.png')
+            }else{
+                //Idoso
+                img.setAttribute('src','foto-idoso-f.png')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 
 }
